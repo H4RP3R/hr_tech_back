a715746a4bbeb6b080013888b69e5e6e83114842
H4RP3R <zombiehunter-vm@yandex.ru>
2020-10-21 21:18:04 +0300

    + QuestionAdmin model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from main_app.models import Profile
+from main_app.models import Profile, Question
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
     pass
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ from main_app.views import QuestionList, QuestionDetail, QuestionnaireList, Ques
 
 urlpatterns = [
     path('questions/', QuestionList.as_view(), name='question_list'),
-    path('question/<int:pk>', QuestionList.as_view(), name='question_detail'),
+    path('questions/<int:pk>', QuestionDetail.as_view(), name='question_detail'),
     path('questionnaire/', QuestionnaireList.as_view(), name='questionnaire_list'),
     path('questionnaire/<int:pk>', QuestionnaireDetail.as_view(), name='questionnaire_detail'),
     path('profiles/', ProfileList.as_view(), name='profile_list'),
