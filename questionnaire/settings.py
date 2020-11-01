aed13d5a6d6d883cab7e247e2f3c6888754517a6
H4RP3R <zombiehunter-vm@yandex.ru>
2020-11-01 19:27:20 +0300

    settings for postgres

--- questionnaire/settings.py
+++ questionnaire/settings.py
@@ -83,12 +83,23 @@ WSGI_APPLICATION = 'questionnaire.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
+    }
 
 
 # Password validation
