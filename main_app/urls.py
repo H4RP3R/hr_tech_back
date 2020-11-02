bfbb25a65362d6a69d1befec41f93d8bc76c45b1
H4RP3R <zombiehunter-vm@yandex.ru>
2020-11-02 13:14:00 +0300

    new urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,28 +6,28 @@ from main_app.views import QuestionList, QuestionDetail, QuestionnaireList, Ques
     UserList, pollResultsListByUserId, PublishedQuestionnaireList
 
 urlpatterns = [
-    path('questions/', QuestionList.as_view(), name='question_list'),
-    path('questions/<int:pk>', QuestionDetail.as_view(), name='question_detail'),
+    path('api/questions/', QuestionList.as_view(), name='question_list'),
+    path('api/questions/<int:pk>', QuestionDetail.as_view(), name='question_detail'),
 
-    path('questionnaire/', QuestionnaireList.as_view(), name='questionnaire_list'),
-    path('questionnaire/<int:pk>', QuestionnaireDetail.as_view(), name='questionnaire_detail'),
-    path('published_questionnaire/',
+    path('api/questionnaire/', QuestionnaireList.as_view(), name='questionnaire_list'),
+    path('api/questionnaire/<int:pk>', QuestionnaireDetail.as_view(), name='questionnaire_detail'),
+    path('api/published_questionnaire/',
          PublishedQuestionnaireList.as_view(), name='published_questionnaire'),
 
-    path('profiles/', ProfileList.as_view(), name='profile_list'),
-    path('profiles/<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
+    path('api/profiles/', ProfileList.as_view(), name='profile_list'),
+    path('api/profiles/<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
 
-    path('current_user_data/', current_user_data, name='current_user_data'),
-    path('user_list/', UserList.as_view(), name='user_list'),
+    path('api/current_user_data/', current_user_data, name='current_user_data'),
+    path('api/user_list/', UserList.as_view(), name='user_list'),
 
     # _____________________________ Stats _______________________________________
-    path('question_stats/', QuestionStatsList.as_view(), name='question_stats'),
-    path('question_stats/<int:pk>', QuestionStatsDetail.as_view(), name='question_stats_detail'),
-    path('poll_results/', PollResultsList.as_view(), name='poll_results_list'),
-    path('poll_results/<int:pk>', PollsResultsDetail.as_view(), name='poll_results_detail'),
-    path('question_in_poll_stats/', QuestionInPollStatsList.as_view(),
+    path('api/question_stats/', QuestionStatsList.as_view(), name='question_stats'),
+    path('api/question_stats/<int:pk>', QuestionStatsDetail.as_view(), name='question_stats_detail'),
+    path('api/poll_results/', PollResultsList.as_view(), name='poll_results_list'),
+    path('api/poll_results/<int:pk>', PollsResultsDetail.as_view(), name='poll_results_detail'),
+    path('api/question_in_poll_stats/', QuestionInPollStatsList.as_view(),
          name='question_in_poll_stats'),
-    path('question_in_poll_stats/<int:pk>', QuestionInPollStatsDetail.as_view(),
+    path('api/question_in_poll_stats/<int:pk>', QuestionInPollStatsDetail.as_view(),
          name='question_in_poll_detail'),
-    path('poll_results_for_user/<int:pk>', pollResultsListByUserId, name='poll_results_by_id')
+    path('api/poll_results_for_user/<int:pk>', pollResultsListByUserId, name='poll_results_by_id')
 ]
